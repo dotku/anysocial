091ccefbbbc90fc68d2d9663896516925503f3fd
jay-itl <weijingjaylin@gmail.com>
2024-08-25 02:19:55 -0700

    update description content ...

--- src/components/Posts/PostCreateInput.tsx
+++ src/components/Posts/PostCreateInput.tsx
@@ -47,7 +47,7 @@ export default function PostCreatInput() {
         disabled={loading}
         variant="faded"
         placeholder="What's on your mind?"
-        description="Hint: share whatever you want to talk about, we don't care if you are a bot 🤖️."
+        description="Hint: share whatever you want to talk about anonymously, we don't care if you are a bot 🤖️."
         onChange={handleInputChange}
       />
       <div className="flex justify-between">
